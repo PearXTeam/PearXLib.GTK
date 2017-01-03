973036bbd5a669311dc1cd32eeaff3438c16950d
mrAppleXZ <mr.applexz@gmail.com>
2017-01-03 18:11:39 +0500

    init

---
+++ PearXLib.GTK/LoadableWindow.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using Gtk;
+
+namespace PearXLib.GTK
+{
+	public class LoadableWindow : PXWindow
+	{
+		public Overlay Overlay = new Overlay();
+		public Widget Loading = null;
+		public bool UseDefaultLoading { get; set; } = true;
+
+		public LoadableWindow()
+		{
+			base.Add(Overlay);
+		}
+
+		public void StartLoading()
+		{
+			Application.Invoke((sender, e) =>
+			{
+				if (UseDefaultLoading && Loading == null)
+					Loading = new Image(new Gdk.PixbufAnimation(ResourceUtils.StreamFromResources("PearXLib.GTK.Resources.Loading.gif")));
+				Overlay.Child.Hide();
+				if (!Overlay.Children.Contains(Loading))
+					Overlay.AddOverlay(Loading);
+				Loading.ShowAll();
+			});
+		}
+
+		public void StopLoading()
+		{
+			Application.Invoke((sender, e) =>
+			{
+				Loading.Hide();
+				Overlay.Child.ShowAll();
+			});
+		}
+
+		public new void Add(Widget w)
+		{
+			Overlay.Add(w);
+		}
+
+
+		public new void Remove(Widget w)
+		{
+			Overlay.Remove(w);
+		}
+	}
+}
---
+++ PearXLib.GTK/PXWindow.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Gtk;
+
+namespace PearXLib.GTK
+{
+	public class PXWindow : Window
+	{
+		public bool Main { get; set; }
+
+		public PXWindow() : base(WindowType.Toplevel)
+		{
+			DeleteEvent += (o, args) =>
+			{
+				if (Main)
+				{
+					Application.Quit();
+				}
+			};
+			Shown += (sender, e) =>
+			{
+				if (Child != null)
+					Child.ShowAll();
+			};
+			SetPosition(WindowPosition.CenterAlways);
+		}
+	}
+}
